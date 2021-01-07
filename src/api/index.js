51bd9d6d784e51a9d0b1ae094a9aa89c7d4dec58
Théo Lavaux <lavauxtheo@gmail.com>
2021-01-07 01:24:11 +0100

    :bulb: write swagger documentation

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 
 const users = require('./users');
 const posts = require('./posts');
@@ -6,14 +7,18 @@ const comments = require('./comments');
 
 const router = express.Router();
 
+const swaggerDocument = require('./swagger.json');
+
 router.get('/', (req, res) => {
   res.json({
-    message: 'API route',
+    message: 'v1 API route',
   });
 });
 
+router.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 router.use('/users', users);
 router.use('/posts', posts);
-router.use('/users', comments);
+router.use('/comments', comments);
 
 module.exports = router;
